d3625eca5d4110ee55e6df62f9ddbf2b7c7755df
Tom Xu <tomxu89@gmail.com>
2018-01-13 07:47:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = []
-  
+  var sentence = 0
+  for (let i = 0; i < musicians.length; i++) {
+    for (j = 0); j < instruments.length; j++) {
+      array[++sentence] = `"${musicians[i]} plays ${istruments[j]}"`
+    }
+  }
 }
