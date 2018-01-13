ba153a448b8a1b91996158cbaf94aa75f7681c60
Tom Xu <tomxu89@gmail.com>
2018-01-13 07:50:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ function theBeatlesPlay(musicians, instruments) {
   var sentence = 0
   for (let i = 0; i < musicians.length; i++) {
     for (j = 0; j < instruments.length; j++) {
-      array[++sentence] = `"${musicians[i]} plays ${istruments[j]}"`
+      array[++sentence] = `${musicians[i]} plays ${istruments[j]}`
     }
   }
+}
+
+function johnLennonFacts(facts) {
+  let i = 0
+  while (i < facts.length) {
+    facts[i] +='!!!'
+    i++
+  }
+}
+
+function iLoveTheBeatles(num) {
+  let array 
 }
